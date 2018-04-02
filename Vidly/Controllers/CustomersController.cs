867682f7c7747485cc3725d44bc74f3ce0d6b687
avalonh <andromeda.avalon@gmail.com>
2018-04-02 14:54:10 +0200

    include object referenced to model

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,23 +5,39 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var cus = GetCustomer();
-            //cus.Customers.Clear();
+            //var cus = GetCustomer();
+
+            var cus = _context.Customers.Include(c => c.MembershipType).ToList();
+            
 
             return View(cus);
         }
 
         public ActionResult Details(int id)
         {
-            var cus = GetCustomer().SingleOrDefault(s => s.Id == id);
+            //var cus = GetCustomer().SingleOrDefault(s => s.Id == id);
+            var cus = _context.Customers.SingleOrDefault(s => s.Id == id);
 
             if (cus == null)
                 return HttpNotFound();
